a743d5304ce5c819a84fdae3cdc2d18294398286
Rahul Bhardwaj <147973917+rahulxbhardwaj@users.noreply.github.com>
2024-04-09 16:23:16 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </main>
+  )
+}
